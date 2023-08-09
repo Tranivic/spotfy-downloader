e50cca39c6112c1c9cc8a6801bbe878c74769070
Tranivic <victor.trani1@gmail.com>
2023-08-08 21:48:55 -0300

    chore: main component is nom function based

--- src/layouts/Main/Main.tsx
+++ src/layouts/Main/Main.tsx
@@ -1,15 +1,34 @@
-import { Component } from "react";
-import "./Main.css";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { usePlaylist } from "../../redux/slicePlaylist";
+import PlaylistObject from "../../types/playlist";
 import InputForm from "../../components/InputForm/InputForm";
 import TrackList from "../../components/TrackList/TrackList";
+import "./Main.css";
 
-export default class Main extends Component {
-  render() {
-    return (
-      <main className="flex flex-col items-center">
-        <InputForm />
-        <TrackList />
-      </main>
-    );
+const Main = () => {
+  const playlistState = useSelector(usePlaylist);
+  const [playlistObject, setPlaylistObject] = useState({});
+
+  function isPlaylistEmpty(obj: object): boolean {
+    return Object.keys(obj).length === 0;
   }
-}
+
+  useEffect(() => {
+    setPlaylistObject({
+      ...playlistState.data,
+    });
+  }, [playlistState.data]);
+
+  return (
+    <main className="flex flex-col items-center">
+      {isPlaylistEmpty(playlistObject) ? (
+        <InputForm />
+      ) : (
+        <TrackList playlist={playlistObject as PlaylistObject} />
+      )}
+    </main>
+  );
+};
+
+export default Main;

f46133aa555723963e91ea75c7a18315050ea93f
Tranivic <victor.trani1@gmail.com>
2023-08-08 21:48:00 -0300

    feat: add track list base behavior

--- src/components/TrackItem/TrackItem.tsx
+++ src/components/TrackItem/TrackItem.tsx
@@ -1,25 +1,28 @@
-import { Component } from "react";
+import React from "react";
+import Track from "../../types/playlist";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCloudArrowDown } from "@fortawesome/free-solid-svg-icons";
-
 import "./TrackItem.css";
 
-export default class TrackItem extends Component {
-  render() {
-    return (
-      <>
-        <li className="hover:scale-105 transition-all duration-200 max-w-xl py-4 px-8 rounded-xl flex justify-between w-full  border-slate-100 border my-2">
-          <div className="track-info">
-            <h1 className="">Respostas</h1>
-            <div className="track-by text-stone-500 text-xs">
-              <span>By: Rodrigo Faro</span>
-            </div>
-          </div>
-          <button className="hover:text-stone-500 transition-all">
-            <FontAwesomeIcon className="" icon={faCloudArrowDown} />
-          </button>
-        </li>
-      </>
-    );
-  }
+interface TrackItemProps {
+  track: Track;
 }
+
+const TrackItem: React.FC<TrackItemProps> = (props) => {
+  console.log(props.track);
+  return (
+    <li className="hover:scale-105 transition-all duration-200 max-w-xl py-4 px-8 rounded-xl flex justify-between w-full  border-slate-100 border my-2">
+      <div className="track-info">
+        <h1 className="">{props.track.name}</h1>
+        <div className="track-by text-stone-500 text-xs">
+          <span>{props.track.album.artists[0].name}</span>
+        </div>
+      </div>
+      <button className="hover:text-stone-500 transition-all">
+        <FontAwesomeIcon className="" icon={faCloudArrowDown} />
+      </button>
+    </li>
+  );
+};
+
+export default TrackItem;
--- src/components/TrackList/TrackList.tsx
+++ src/components/TrackList/TrackList.tsx
@@ -1,16 +1,26 @@
-import { Component } from "react";
+import React from "react";
 import TrackItem from "../TrackItem/TrackItem";
+import PlaylistObject from "../../types/playlist";
+import Track from "../../types/playlist";
 import "./TrackList.css";
 
-export default class TrackList extends Component {
-  render() {
-    return (
-      <div className="list-component mt-10 p-2 flex flex-col items-center justify-center list-container w-screen">
-        <h1 className="text-2xl pb-4">Playlist</h1>
-        <ul className="w-full p-6 flex flex-col items-center justify-center">
-          <TrackItem />
-        </ul>
-      </div>
-    );
-  }
+interface TrackListProps {
+  playlist: PlaylistObject;
 }
+
+const TrackList: React.FC<TrackListProps> = (props) => {
+  const playlist: PlaylistObject = props.playlist;
+
+  return (
+    <div className="list-component mt-10 p-2 flex flex-col items-center justify-center list-container w-screen">
+      <h1 className="text-2xl pb-4">Playlist</h1>
+      <ul className="w-full p-6 flex flex-col items-center justify-center">
+        {Object.values(playlist.items).map((track: Track) => (
+          <TrackItem key={track.id} track={track} />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default TrackList;
